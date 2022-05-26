409416caca669d1d55f464180b4acad550345cae
lizx <lizx@nbport.com.cn>
2022-05-27 00:01:31 +0800

    初始化项目

---
+++ user-service/src/main/java/com/guapixu/user/UserApplication.java
@@ -0,0 +1,14 @@
+package com.guapixu.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lizx
+ */
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class,args);
+    }
+}
